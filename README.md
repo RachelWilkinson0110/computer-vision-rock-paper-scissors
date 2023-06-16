e76767743c81d492fa1a59b1770211b36c07ceea
RachelWilkinson0110 <rachel.wilkinson51@yahoo.com>
2023-06-16 14:15:06 +0100

    Milestone 4 completed'

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Computer Vision RPS
+
+I created a separate environemnt named CV and specified that this environment should use version 3.8 of python. I did this using conda create -n "CV" python =3.8.
+
+I then created 3 separate functions - get_computer_choice, get_user_choice and get_winner.
+get_computer_choice uses the random module (random.choice()) to choose a random option from the list rock, paper, scissors.
+
+get_user_choice takes user input for either rock, paper or scissors.
+
+get_winner uses both the computer_choice and user_choic to determine who wins the game. The relevant output is then printed - 'It's a tie', 'You win' or 'You lose'.
+
+I then called this 3 functions within one final function - play().
+
---
+++ manual_rps.py
@@ -0,0 +1,46 @@
+import random
+
+def get_computer_choice():
+    options=["rock", "paper", "scissors"]
+    global computer_choice
+    computer_choice=random.choice(options)
+    
+
+
+
+
+def get_user_choice():
+    global user_choice
+    user_choice=input("Please choose rock, paper or scissors?")
+    
+
+    
+
+def get_winner(computer_choice, user_choice):
+
+    if computer_choice==user_choice:
+        print("It's a tie")
+    elif computer_choice=="rock":
+        if user_choice=="paper":
+            print("You win!")
+        else:
+            print("You lose")
+    elif computer_choice=="scissors":
+        if user_choice=="rock":
+            print("You win!")
+        else:
+            print("You lose")
+    elif computer_choice=="paper":
+        if user_choice=="scissors":
+            print("You win!")
+        else:
+            print("You lose")
+
+
+
+def play():
+    get_computer_choice()
+    get_user_choice()
+    get_winner(computer_choice, user_choice)
+
+play()
